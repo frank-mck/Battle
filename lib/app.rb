9da9fb3ad96cd5dcdc75369f2dc82a11b9d9c672
Frank Mckenna <fmckenna56330@gmail.com>
2021-06-07 16:16:28 +0100

    Sinatra: Returning HTML

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "hello there"
+end
+
+get '/home' do
+  'Home page'
+end
+
+get '/cat' do
+  "<div>
+  <img class = 'cat' src = 'https://i.imgur.com/jFaSxym.png'>
+  </div>
+
+  <style>
+  .cat {
+    border: 3px dashed red
+  }
+  </style>
+  "
 end
