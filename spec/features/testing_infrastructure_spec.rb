dc944dc8fbce60ce8c38fa4ea0ef4abb97733ce2
Frank Mckenna <fmckenna56330@gmail.com>
2021-06-08 19:26:27 +0100

    started a fight by entering names

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,12 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+feature 'Enter names' do
+  scenario 'submitting names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player_1_name, with: 'Charlotte'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    
+    save_and_open_page # will save the web page and open the browser to display it
+    
+    expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
