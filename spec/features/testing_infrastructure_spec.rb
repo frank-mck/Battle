009eadd21be921ce69e3b07c216603c9f7567c62
Frank Mckenna <fmckenna56330@gmail.com>
2021-06-09 17:22:07 +0100

    player 2 hit points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -10,3 +10,13 @@ feature 'Enter names' do
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
+
+  feature 'View hit points' do
+    scenario 'view player 2 hit points' do
+      visit('/')
+      fill_in :player_1_name, with: 'Charlotte'
+      fill_in :player_2_name, with: 'Mittens'
+      click_button 'Submit'
+      expect(page).to have_content 'Mittens [100HP]'
+    end
+end
