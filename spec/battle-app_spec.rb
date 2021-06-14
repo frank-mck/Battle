73aed59f5e253bf44fdfc227c708192aa0551ac4
Frank Mckenna <fmckenna56330@gmail.com>
2021-06-14 14:20:43 +0100

    refactored

--- spec/battle-app_spec.rb
+++ spec/battle-app_spec.rb
@@ -3,7 +3,7 @@ feature 'Attacking' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content 'Charlotte attacked Mittens'
-    click_link 'OK'
+    click_button 'OK'
     click_button 'Attack'
     expect(page).to have_content 'Mittens attacked Charlotte'
   end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,14 +1,17 @@
 feature 'Attacking' do
-  scenario 'get confirmation of attack' do
+  scenario 'get confirmation of attack from both players' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content 'Charlotte attacked Mittens'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content 'Mittens attacked Charlotte'
   end
 
 scenario 'reduce Player 2 HP by 10' do
   sign_in_and_play
   click_button 'Attack'
-  click_link 'OK'
+  click_button 'OK'
   expect(page).not_to have_content 'Mittens: 60HP'
   expect(page).to have_content 'Mittens: 50HP'
 end
@@ -16,7 +19,7 @@ end
 scenario 'reduce Player 1 HP by 10' do
   sign_in_and_play
   click_button 'Attack'
-  click_link 'OK'
+  click_button 'OK'
   expect(page).not_to have_content 'Mittens: 60HP'
   expect(page).to have_content 'Mittens: 50HP'
 end
